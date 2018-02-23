cfe1feab348636dc157b8ca15b232c72a35ec0c2
PeterJoelMiller <peterjoelmiller@gmail.com>
2018-02-22 19:15:42 -0800

    Update form layout

--- NotePractice/Form1.Designer.cs
+++ NotePractice/Form1.Designer.cs
@@ -1374,7 +1374,7 @@
             // txtTimer1
             // 
             this.txtTimer1.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtTimer1.Location = new System.Drawing.Point(929, 36);
+            this.txtTimer1.Location = new System.Drawing.Point(933, 35);
             this.txtTimer1.Name = "txtTimer1";
             this.txtTimer1.Size = new System.Drawing.Size(38, 31);
             this.txtTimer1.TabIndex = 135;
